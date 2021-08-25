f4e4e238a3b635c2a35220e95e7357a16c2aa486
edmariooliver <edmariooliver@outlook.com>
2021-08-25 18:17:05 -0300

    Novo layout

--- assets/js/configWs.js
+++ assets/js/configWs.js
@@ -1,59 +1,66 @@
-// var i = setInterval(function () {
+// var msg = new WebSocket("wss://chat-day.herokuapp.com/wss");
 
-//     clearInterval(i);
+var nodebr = document.createElement("br");
+var msg = 0
 
-//     // O código desejado é apenas isto:
-//     document.getElementById("loading").style.display = "none";
-//     document.getElementById("div-form").style.display = "inline";
-
-// }, 4000);
-var msg = new WebSocket("wss://chat-day.herokuapp.com/wss");
+console.log(onsubmit)
+msg = new WebSocket("ws://192.168.0.100:8085");
 msg.onopen = function(e) {
     console.log("Sucesso")
 }
-;
-var nodebr = document.createElement("br");
 
 msg.onmessage = function(e) {
-
     dados = JSON.parse(e.data);
     if (dados.type == "bot") {
         if (dados.typemsg == "txt") {
+            // var divMsg = document.getElementById("divMsg")
+            // console.log(dados.nome)
+            // divMsg.innerHTML += '<div class="div-msg-other"><p class="nome-other '+dados.type+'"><strong>'+ dados.nome +'</strong></p><p class="msg-other">'+ dados.msg +'</p></div>'
+            
             var nodeNome = document.createElement("p");
             nodeNome.className = "nome-other";
             var nodeMsg = document.createElement("p");
+            var strong = document.createElement("strong");
+            strong.appendChild(nodeNome)
             nodeMsg.setAttribute("style", "padding:15px;");
-            var textnodeNome = document.createTextNode(dados.nome + "🤖");
+            var textnodeNome = document.createTextNode(dados.nome);
             nodeNome.appendChild(textnodeNome);
             var textnodeMsg = document.createTextNode(dados.msg);
             nodeMsg.appendChild(textnodeMsg);
             nodeMsg.className='msg-other'
-            nodeNome.className='nome-bot'
+            nodeNome.className='bot'
             var divNode = document.createElement("div")
             divNode.className = 'div-msg-other'
-            divNode.appendChild(nodeNome)
+            divNode.appendChild(strong)
             divNode.appendChild(nodeMsg)
             document.querySelector('.mensagens').appendChild(divNode)
         }
         if (dados.typemsg == "video") {
-            
+            // var divMsg = document.getElementById("divMsg")
+            // divMsg.innerHTML += '<div class="div-msg-other"><p class=" nome-other '+dados.type+'"><strong>'+dados.nome+'<strong></p><iframe src="'+dados.msg+'" width="290" height="400" allowfullscreen="" class="msg-video"></iframe></div>'
             var nodeNome = document.createElement("p");
-            var textnodeNome = document.createTextNode(dados.nome + "🤖");
+            var textnodeNome = document.createTextNode(dados.nome);
             nodeNome.appendChild(textnodeNome);
             iframe = document.createElement("iframe");
             iframe.setAttribute("src", dados.msg);
             iframe.setAttribute("width", 290);
             iframe.setAttribute("height", 400);
             iframe.setAttribute("allowfullscreen", "");
-            nodeNome.className = "nome-bot"
+            nodeNome.className = "bot"
             iframe.className = "msg-video"
             var divNode = document.createElement("div")
             divNode.className = 'div-msg-other'
-            divNode.appendChild(nodeNome)
+            var nodeMsg = document.createElement("p");
+            var strong = document.createElement("strong");
+            strong.appendChild(nodeNome)
+            divNode.appendChild(strong)
             divNode.appendChild(iframe)
             document.querySelector('.mensagens').appendChild(divNode)
         }
     } else {
+        // var divMsg = document.getElementById("divMsg")
+        // console.log(dados.nome)
+        // divMsg.innerHTML += '<div class="div-msg-other"><p class="nome-other"><strong>'+ dados.nome +'</strong></p><p class="msg-other">'+ dados.msg +'</p></div>'
         var nodeNome = document.createElement("p")
         nodeNome.className = "nome-other"
         var textnodeNome = document.createTextNode(dados.nome);
@@ -63,57 +70,47 @@ msg.onmessage = function(e) {
         nodeMsg.className="msg-other"
         var textnodeMsg = document.createTextNode(dados.msg);
         nodeMsg.appendChild(textnodeMsg)
-
+        var strong = document.createElement("strong");
+        strong.appendChild(nodeNome)
         var divNode = document.createElement("div")
-        divNode.appendChild(nodeNome)
+        divNode.className = "div-msg-other"
+        divNode.appendChild(strong)
         divNode.appendChild(nodeMsg)
 
         document.querySelector(".mensagens").appendChild(divNode);
     }
-    if(document.getElementById('autosc').checked == true){        
-        console.log(document.querySelector(".mensagens").scrollTop = document.querySelector(".mensagens").scrollHeight)   
-    }
-    if(document.getElementById('notify').checked == true){        
-        var audio = document.getElementById("audio");
-        audio.play()
-    }
+    // if(document.getElementById('notify').checked == true){        
+    //     var audio = document.getElementById("audio");
+    //     audio.play()
+    // }
+    console.log(document.querySelector(".mensagens").scrollTop = document.querySelector(".mensagens").scrollHeight)  
 }
 
 function sendMsgEnter(event) {
-    
     if (event.key == "Enter") {
         sendMsg()
     }
 }
 function sendMsg(n) {
 
-    var nome = document.querySelector("#input-name").value;
+    var divMsg = document.getElementById("divMsg")
     var msgSend = document.getElementById("msg").value;
     var msgValid = msgSend.trim()
-
     if (msgValid.length > 99 || msgValid.length <= 0) {
-
         window.alert("Mensagens com menos de 1 caractere ou mais de 100, não são válidas")
     } else {
-
         var dados = {
-            nome: nome,
             msg: msgSend
         };
-
         dados = JSON.stringify(dados);
         msg.send(dados)
-
-        var nodeNome = document.createElement("p")
-        nodeNome.className = "nome-me"
-        var textnodeNome = document.createTextNode(nome);
-        nodeNome.appendChild(textnodeNome)
+        // divMsg.innerHTML += '<div class="div-msg-me"><p class="msg-me">'+msgSend+'</p></div>'
+        // document.getElementById("msg").value = "" 
 
         var nodeMsg = document.createElement("p")
         nodeMsg.className="msg-me"
         var textnodeMsg = document.createTextNode(msgSend);
         nodeMsg.appendChild(textnodeMsg)
-
         var divNode = document.createElement("div")
         divNode.className = "div-msg-me"
         divNode.appendChild(nodeMsg)
@@ -121,9 +118,21 @@ function sendMsg(n) {
         document.querySelector(".mensagens").appendChild(divNode);
 
         document.getElementById("msg").value = ""
-
-        if(document.getElementById('autosc').checked == true){        
-            console.log(document.querySelector(".mensagens").scrollTop = document.querySelector(".mensagens").scrollHeight)   
-        }
     }
+    console.log(document.querySelector(".mensagens").scrollTop = document.querySelector(".mensagens").scrollHeight)  
 }
+
+msg.onError = function (e){
+    window.alert("erro")
+}
+function del_msg(){
+    var node = document.querySelector(".mensagens");
+    if (node.parentNode) {
+        node.parentNode.removeChild(node);
+    }
+    var nodechat = document.createElement('div')
+    nodechat.className = 'mensagens'
+    var nodedivchat = document.querySelector('.chat')
+    nodedivchat.appendChild(nodechat)
+}
+
--- assets/js/script_menu.js
+++ assets/js/script_menu.js
@@ -1,16 +1,9 @@
-
-function del_msg(){
-    var node = document.querySelector(".mensagens");
-    if (node.parentNode) {
-        node.parentNode.removeChild(node);
-    }
-    var nodechat = document.createElement('div')
-    nodechat.className = 'mensagens'
-    var nodedivchat = document.querySelector('.chat')
-    nodedivchat.appendChild(nodechat)
-
-}
 function wel_chat(){
+    var nomeInput = (document.querySelector('#input-name').value)
+    nome = nomeInput.trim()
+
+    // msg = new WebSocket("ws://192.168.0.100:8085?name="+nome);
+    //iniciando a conexao
     var form = document.querySelector('.div-form')
     var chat = document.querySelector('.grid')
     var nomeInput = (document.querySelector('#input-name').value)
@@ -20,9 +13,8 @@ function wel_chat(){
         window.alert('Nome inválido, seu nome deve conter de 1 até 15 caracteres')
  
     }else{
-        var name_user = document.getElementById('name-user')
-        form.style.display = 'none'
-        chat.style.display = 'grid'
+        document.cookie = "@" + nome + "@";
+        window.location.href = '/chat.html'
     }
 
 }
